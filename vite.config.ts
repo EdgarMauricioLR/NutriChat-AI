7a7c4112c7d70a57a6221fc635203680f02995b6
EdgarMauricioLR <EdgarMauricioLR@gmail.com>
2025-12-02 14:49:23 -0500

    Tabula rasa

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // IMPORTANTE: Esta línea le dice a la app que vive en esta subcarpeta en GitHub
-  base: '/NutriChat-AI/', 
-})
